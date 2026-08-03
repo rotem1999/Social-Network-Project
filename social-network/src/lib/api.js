bdcf7498b059d82b7f2582155c32c7269a7268b5
Rotem <rotembar38@gmail.com>
2026-08-03 18:06:18 +0300

    Feat (frontend/api) - added path to CHATS

--- social-network/src/lib/api.js
+++ social-network/src/lib/api.js
@@ -4,3 +4,5 @@ export const USERS_URL = API_URL + "/api/users";
 export const GROUPS_URL = API_URL + "/api/groups";
 export const POSTS_URL = API_URL + "/api/posts";
 export const COMMENTS_URL = API_URL + "/api/comments";
+export const CHAT_URL = API_URL + "/api/chat";
+export const SOCKET_URL = API_URL;
